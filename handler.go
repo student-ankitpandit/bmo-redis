99aa47e99699d69d85811c784fc2702007f0bd89
student-ankitpandit <ankitpanditwork120@gmail.com>
2026-09-20 18:23:12 +0530

    write hset and hget methods

--- handler.go
+++ handler.go
@@ -1,6 +1,9 @@
 package main
 
-import "sync"
+import (
+	"hash"
+	"sync"
+)
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -14,10 +17,16 @@ var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 	"SET": set,
 	"GET": get,
+	"HSET": hset,
+	"HGET": hget,
+
 }
 
 var SETs = map[string]string{}
 var SETsMu = sync.RWMutex{}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutux{}
 
 func set(args []Value) Value {
 	if len(args) != 2 {
@@ -51,3 +60,56 @@ func get(args []Value) Value {
 	return Value{typ: "bulk", bulk: value}
 }
 
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "err wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash][key]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+
+	HSETs[hash][key] = value
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "err wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[0].bulk
+
+	HSETsMu.Lock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.Unlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bluk", bulk: value}
+}
+
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "err wrong number of arguments for 'hgetall' command"}
+	}
+
+	hash := args[0].bulk
+
+	HSETsMu.Lock()
+	key, ok := SETs[hash]
+	value := SETs[key]
+	HSETsMu.Unlock()
+	
+	
+}
